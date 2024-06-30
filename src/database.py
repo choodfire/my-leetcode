eb561b4f9dd700c3ac84e788e1059d304e5d34d0
Ruslan <khalitovr21@gmail.com>
2024-06-30 23:03:21 +0500

    Created class to work with DB & sessions

--- src/database.py
+++ src/database.py
@@ -1,23 +1,43 @@
-from collections.abc import AsyncGenerator
+from asyncio import current_task
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
 
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncConnection, AsyncSession, \
+    async_scoped_session
 
 from src.config import settings
 
-engine = create_async_engine(
-    settings.asyncpg_url.unicode_string(),
-    future=True,
-    echo=True,  # TODO: Remove in future
-)
 
-AsyncSessionFactory = async_sessionmaker(
-    engine,
-    autoflush=False,
-    autocommit=False,
-    expire_on_commit=False,
-)
+class DatabaseSessionManager:
+    def __init__(self) -> None:
+        self.engine = create_async_engine(
+            settings.asyncpg_url.unicode_string(),
+            future=True,
+            echo=True,  # TODO: Remove in future
+        )
+        self.sessionmaker = async_sessionmaker(
+            self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.sessionmaker,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session(self) -> AsyncSession:
+        async with self.sessionmaker() as session:
+            yield session
+            await session.close()
 
 
-async def get_db() -> AsyncGenerator:
-    async with AsyncSessionFactory() as session:
+sessionmanager = DatabaseSessionManager()
+
+
+async def get_session():
+    async with sessionmanager.session() as session:
         yield session
